add1f57aa5768529c6fc2654c452ff606d153c2a
Coen Mooij <coenmooij@gmail.com>
2018-04-10 14:59:01 +0200

    fix namespace config

--- config/app.php
+++ config/app.php
@@ -155,12 +155,8 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-        App\Providers\DependencyInjectionServiceProvider::class,
+        CoenMooij\BrandApi\Infrastructure\Providers\RouteServiceProvider::class,
+        CoenMooij\BrandApi\Infrastructure\Providers\DependencyInjectionServiceProvider::class,
 
     ],
 
